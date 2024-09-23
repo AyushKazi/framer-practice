8be361cfee3c3511e89d92748c87c55a32f4d21e
AyushKazi <ayushkazishrestha@gmail.com>
2024-09-23 20:00:55 +0545

    feat: lenis scroll added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-import Image from "next/image";
+"use client";
 import ZoomParallax from "../components/ZoomParallax";
 import Box from "../components/Box";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <div>
